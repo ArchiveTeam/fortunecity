2d2673edfe84a400e9d2789fcad60511454cfa69
Alard <alard@example.com>
2012-03-04 19:52:42 +0100

    Download-a-street script.

---
+++ dld-street.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+VERSION="20120304.01"
+
+tld=$1
+area=$2
+street=$3
+
+USER_AGENT="Googlebot/2.1 (+http://www.googlebot.com/bot.html)"
+USER_AGENT="Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27"
+
+area_dir="data/$tld/$area"
+street_dir="$area_dir/$street"
+
+if [ -d "$area_dir" ] && [ ! -z "$( find "$area_dir/" -maxdepth 1 -type f -name "$tld-$area-$street-*.warc.gz" )" ]
+then
+  echo "Already downloaded ${tld}/${area}/${street}"
+  exit 2
+fi
+
+rm -rf "$street_dir"
+
+mkdir -p "$street_dir"
+mkdir -p "$street_dir/files"
+
+echo "http://www.fortunecity.$tld/$area/$street/" > "$street_dir/urls.txt"
+for i in $( seq 0 2600 )
+do
+  echo "http://www.fortunecity.$tld/$area/$street/$i/"
+done >> "$street_dir/urls.txt"
+
+t=$( date -u +'%Y%m%d-%H%M%S' )
+warc_file_base="$tld-$area-$street-$t"
+
+./wget-warc \
+  -U "$USER_AGENT" \
+  -nv \
+  -o "$street_dir/$tld-$area-$street-$t.log" \
+  -r -l inf --no-remove-listing \
+  --page-requisites \
+  --no-parent \
+  --trust-server-names \
+  -i "$street_dir/urls.txt" \
+  --directory-prefix="$street_dir/files/" \
+  --warc-file="$street_dir/$warc_file_base" \
+  --warc-header="operator: Archive Team" \
+  --warc-header="fortunecity-dld-script-version: ${VERSION}" \
+  --warc-header="fortunecity: ${tld}, ${area}, ${street}"
+
+result=$?
+
+if [ $result -ne 0 ] && [ $result -ne 6 ] && [ $result -ne 8 ]
+then
+  echo " ERROR ($result)."
+  exit 1
+fi
+
+mv "$street_dir/$warc_file_base.warc.gz" "$area_dir/$warc_file_base.warc.gz"
+rm -rf "$street_dir"
+
