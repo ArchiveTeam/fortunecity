285b3166a78172cbfa2b7918dc3b75072de14672
Alard <alard@example.com>
2012-03-06 20:14:32 +0100

    Upload script.

---
+++ upload-finished.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# Upload complete files to the Fortress of Solitude.
+#
+# This script will look in your data/ directory to find
+# users that are finished. It will upload the data for
+# these users to the repository using rsync.
+#
+# You can run this while you're still downloading,
+# since it will only upload data that is done.
+#
+# Usage:
+#   ./upload-finished.sh $YOURNICK
+#
+# You can set a bwlimit for rsync, e.g.:
+#   ./upload-finished.sh $YOURNICK 300
+#
+
+destname=$1
+target=fos
+dest=${target}.textfiles.com::fortunecity/$1/
+if [ -z "$destname" ]
+then
+  echo "Usage:  $0 [yournick] [bwlimit]"
+  exit
+fi
+if [[ ! $destname =~ ^[-A-Za-z0-9_]+$ ]]
+then
+  echo "$dest does not look like a proper nickname."
+  echo "Usage:  $0 [yournick] [bwlimit]"
+  exit
+fi
+
+bwlimit=$2
+if [ -n "$bwlimit" ]
+then
+  bwlimit="--bwlimit=${bwlimit}"
+fi
+
+# note: DO NOT USE --partial !
+cd data/
+ls -1 */members/?/??/???/*.warc.gz */*/*.warc.gz | \
+rsync -avz \
+      --compress-level=9 \
+      --progress \
+      ${bwlimit} \
+      --recursive \
+      --files-from="-" \
+      ./ ${dest}
+
+exit 0
+
